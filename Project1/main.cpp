2486c094c55b4a6b1394c44b9b2c4307beb5139a
adcd <adcd@gmail.com>
2017-04-13 11:58:41 +0800

    Commit to the branch_example

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -2,7 +2,10 @@
 using namespace std;
 
 int main() {
-	cout << "hello world!" << endl;
+	cout << "hello world!\n";
+	cout << "Adding a second line to the Hellow World\n";
+	cout << "Add the third line\n";
+	cout << "Demonstrating the use of branch\n";
 	system("pause");
 	return 0;
 }
